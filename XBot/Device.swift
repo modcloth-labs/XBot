dad55cfff5872942d810672a7dc75f850b13d036
Geoffrey Nix <g.nix@modcloth.com>
2014-09-30 15:35:39 -0700

    add dummy function call for testing

---
+++ XBot/Device.swift
@@ -0,0 +1,13 @@
+//
+//  Device.swift
+//  XBot
+//
+//  Created by Geoffrey Nix on 9/30/14.
+//  Copyright (c) 2014 ModCloth. All rights reserved.
+//
+
+import Foundation
+
+public func foo() {
+    println("foo")
+}
